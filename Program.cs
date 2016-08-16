b7a29e389c668dca5de1f94b4b70163f106b68ff
yiming lin <yiming.lin@outlook.com>
2016-08-16 22:39:18 +0800

    Initial Empty csharp dynamic sample

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpDynamic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
