20ee86339fe66eab634808df788d006bda867a31
yiming lin <yiming.lin@outlook.com>
2016-08-17 00:20:17 +0800

    Completed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
+using static System.Console;
+
 namespace CsharpDynamic
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OutputTimeStaticBinding();
+            OutputTimeDynamicBinding();
+            OutputTimeDynamicBindingRunTimeError();
+            
+            WriteLine("\n Press enter to leave...");
+            ReadLine();
+        }
+        public static void OutputTimeStaticBinding()
+        {
+            DateTime dt = DateTime.Now;
+            string time = dt.ToString("yyyy-MM-dd");
+
+            WriteLine(time);
+        }
+        public static void OutputTimeDynamicBinding()
+        {
+            dynamic dt = DateTime.Now;
+            string time = dt.ToString("yyyy-MM-dd");
+
+            WriteLine(time);
+        }
+
+        public static void OutputTimeDynamicBindingRunTimeError()
+        {
+            dynamic dt = DateTime.Now;
+            string time = dt.IsCoffeeTime();
         }
     }
 }
